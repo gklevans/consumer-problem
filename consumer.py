e395636affea5756c0d02f5a07d43cb9fae0ee14
gklevans <gklevans7@gmail.com>
2023-11-13 12:23:13 -0800

    Initial commit

---
+++ consumer.py
@@ -0,0 +1,213 @@
+from matplotlib.figure import Figure
+import numpy as np
+
+from sympy import (
+    Function, 
+    simplify, 
+    solve, 
+    symbols,
+    )
+
+x, y = symbols('x y')
+
+class CES(Function):
+    @classmethod
+    def eval(cls, 
+             x: float, 
+             y: float, 
+             a: float, 
+             p: float,
+             ) -> float:
+        """
+        
+
+        Parameters
+        ----------
+        x : float
+            The amount purchased of Good X.
+        y : float
+            The amount purchased of Good Y.
+        a : float
+            A parameter of the CES utility function.
+        p : float
+            A parameter of the CES utility function.
+
+        Raises
+        ------
+        ValueError
+            'a' and 'p' must be within the ranges required for CES utility.
+
+        Returns
+        -------
+        float
+            The utility for the given parameters and amounts purchased.
+
+        """
+
+        if a <= 0 or a >= 1:
+            raise ValueError("'a' must be strictly between 0 and 1")
+        if p > 1:
+            raise ValueError("'p' must be less than or equal to 1")
+        if p == 0: #Cobb-Douglas
+            return x**a * y**(1-a)
+        if p <= 1: #general CES
+            return (a*x**p + (1-a)*y**p)**(1/p)
+
+class ConsumerProblem:
+    def __init__(self, 
+                 a: float, 
+                 p: float, 
+                 px: float, 
+                 py: float, 
+                 m: float,
+                 ) -> None:
+        """
+        
+
+        Parameters
+        ----------
+        a : float
+            A parameter of the CES utility funciton.
+        p : float
+            A parameter of the CES utility function.
+        px : float
+            The price of Good X.
+        py : float
+            The price of Good Y.
+        m : float
+            The consumer's income.
+
+        Raises
+        ------
+        ValueError
+            The prices and income must be positive.
+
+        Returns
+        -------
+        None
+            Sets the attributes of the ConsumerProblem class.
+
+        """
+
+        if px <= 0 or py <= 0:
+            raise ValueError("'px' and 'px' must be positive")
+        if m <= 0:
+            raise ValueError("'m' must be positive")
+        self.a = a
+        self.p = p
+        self.px = px
+        self.py = py
+        self.m = m        
+        self.f = CES.eval(x, y, self.a, self.p)
+    
+    def utility(self, x: float, y: float) -> float:
+        """
+        
+
+        Parameters
+        ----------
+        x : float
+            The amount purchased of Good X.
+        y : float
+            The amount purchased of Good Y.
+
+        Returns
+        -------
+        float
+            The utility obtained from these amounts purchased.
+
+        """
+        return CES.eval(x=x, y=y, a=self.a, p=self.p)
+
+    def indiff_curve(self, x: np.ndarray, c: float) -> np.ndarray:
+        """
+        
+
+        Parameters
+        ----------
+        x : np.ndarray
+            A numpy array of values for amounts purchased of Good X.
+        c : float
+            The utility associated with the indifference curve.
+
+        Returns
+        -------
+        np.ndarray
+            The y-values of the indifference curve over the given x-values,
+            where they are defined.
+
+        """
+        if self.p == 0:
+            return (c*x**(-self.a))**(1/(1-self.a))
+        base = (c**self.p - self.a*x**self.p)/(1-self.a)
+        return np.piecewise(base, 
+                            [base<=0, base>0], 
+                            [None, lambda base: base**(1/self.p)])
+    
+    def budget_cons(self, x: np.ndarray) -> np.ndarray:
+        """
+        
+
+        Parameters
+        ----------
+        x : np.ndarray
+            A numpy array of values for amounts purchased of Good X.
+
+        Returns
+        -------
+        np.ndarray
+            The y-values of the budget line over the given x-values.
+
+        """
+        return (self.m - self.px * x) / self.py
+    
+    def tangency(self) -> dict:
+        """
+        
+
+        Returns
+        -------
+        dict
+            A dictionary associating 'x' and 'y' to their optimal values
+            for this consumer problem.
+
+        """
+        return solve([simplify(self.f.diff(x)/self.f.diff(y)) 
+                      - self.px/self.py, self.px*x + self.py*y - self.m
+                      ],
+                     (x, y), dict=True)[0]
+    
+    def plot(self) -> Figure:
+        """
+        
+
+        Returns
+        -------
+        Figure
+            A figure plotting the solution of the consumer problem.
+            The indifference curve that is tangent to the budget line
+            is plotted, along with two indifference curves below and two above.
+            Axes limits are set so that the slope of the budget line is clear.
+
+        """
+        lim_y = 1.1 * self.budget_cons(0)
+        x_intercept = solve((self.m - self.px * x) / self.py, x)[0]
+        lim_x = 1.1 * x_intercept
+        lim = max(float(lim_y), float(lim_x))        
+        optimal_bundle = self.tangency()
+        x_opt, y_opt = float(optimal_bundle[x]), float(optimal_bundle[y])
+        opt_util = self.utility(x=x_opt, y=y_opt)
+        c_list = [
+            (1/3)*opt_util, 
+            (2/3)*opt_util, 
+            opt_util, 
+            (4/3)*opt_util,
+            (5/3)*opt_util,
+            ]
+        x_vals = np.linspace(1e-10, lim, 1_000)
+        fig = Figure()
+        ax = fig.add_axes(rect=(0.1,0.1,0.8,0.8), xlim=(0, lim), ylim=(0, lim))
+        for c in c_list:
+            ax.plot(x_vals, self.indiff_curve(x_vals, c), color='orange')
+        ax.plot(x_vals, self.budget_cons(x_vals), color='blue')
+        return fig
---
+++ webapp.py
@@ -0,0 +1,130 @@
+import base64
+from io import BytesIO
+
+from flask import (
+    Flask, 
+    render_template,
+    request,
+    )
+from sympy import symbols
+
+from consumer import ConsumerProblem
+
+app = Flask(__name__)
+
+def convert_to_mathml(html: str) -> str:
+    """
+    
+
+    Parameters
+    ----------
+    html : str
+        HTML that includes the Python parameters 'a', 'p', 'px', 'py', 'm'.
+
+    Returns
+    -------
+    str
+        The same HTML with the parameters mentioned above converted to MathML.
+
+    """
+    
+    MATHML_DICT = {
+        "'a'": """<math><mi>a</mi></math>""",
+        "'p'": "<math><mi>&rho;</mi></math>",
+        "'m'": "<math><mi>m</mi></math>",
+        "'px'": """<math>
+                        <msub>
+                            <mi>p</mi>
+                            <mi>x</mi>
+                        </msub>
+                    </math>""",
+        "'py'": """<math>
+                        <msub>
+                            <mi>p</mi>
+                            <mi>y</mi>
+                        </msub>
+                    </math>""",
+        "'m'": "<math><mi>m</mi></math>",
+        }
+    
+    for key, value in MATHML_DICT.items():
+        html = html.replace(key, value)
+        
+    return html
+
+@app.route('/', methods=['GET','POST'])
+def solver() -> str:
+    '''
+    
+
+    Returns
+    -------
+    str
+        The HTML of the webpage, including the form and results generated.
+
+    '''
+    
+    if request.method == 'GET':
+        return render_template('solver.html')
+    try:
+        a = float(request.form.get('a'))
+        p = float(request.form.get('p'))
+        px = float(request.form.get('px'))
+        py = float(request.form.get('py'))
+        m = float(request.form.get('m'))
+        
+        input_ = f"""You entered the parameters 
+                    'a'={a},
+                    'p'={p},
+                    'px'={px},
+                    'py'={py},
+                    and
+                    'm'={m}."""
+        input_ = convert_to_mathml(input_)
+        
+        
+    except ValueError:
+        solution = "Entries must be numbers."
+        return render_template('solver.html',
+                               solution=solution,
+                               )
+        
+    try:
+        CP = ConsumerProblem(a=a, p=p, px=px, py=py, m=m)
+    
+        try:
+            optimal_bundle = CP.tangency()
+            x, y = symbols('x y')
+            x_opt, y_opt = optimal_bundle[x], optimal_bundle[y]
+            solution = f"""The consumer buys {x_opt:.2f} units of Good X and 
+                        {y_opt:.2f} units of Good Y."""
+            fig = CP.plot()
+            buf = BytesIO()
+            fig.savefig(buf, format="png")
+            data = base64.b64encode(buf.getbuffer()).decode("ascii")
+            img = f"<img src='data:image/png;base64,{data}'/>"
+            
+        except (IndexError, KeyError) as e:
+            print(e)
+            solution = """This tool is currently unable to solve the consumer 
+                          problem for these parameters."""
+            return render_template('solver.html', 
+                                   solution=solution,
+                                   )
+    
+    except ValueError as e:
+        solution = convert_to_mathml(str(e))
+
+        return render_template('solver.html', 
+                               input_=input_, 
+                               solution=solution,
+                               )
+        
+    return render_template('solver.html', 
+                           input_=input_, 
+                           solution=solution, 
+                           img=img,
+                           )
+    
+if __name__ == "__main__":
+    app.run()
